a5a2085e256ea983fc83deb8dfd0bf764bfac9af
lnx1337 <dev.lnx1337@gmail.com>
2017-07-09 16:57:01 -0500

    Se modifica main file para  API

--- main.go
+++ main.go
@@ -1,17 +1,64 @@
 package main
 
 import (
-	"log"
-	"resuelve/invoice"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	ep "resuelve/api/endpoint"
+	s "resuelve/api/service"
+
+	"github.com/bmizerany/pat"
+	"github.com/go-kit/kit/log"
+	httpTransport "github.com/go-kit/kit/transport/http"
+	"github.com/lnx1337/go/api"
+	"github.com/lnx1337/go/config"
+)
+
+const (
+	configFile = `service.invoice.toml`
+)
+
+type settings struct {
+	Server map[string]string
+}
+
+var (
+	cfg          settings
+	listen       = flag.String("listen", ":8080", "Address to listen on")
+	invSvc       = s.InvoiceSvc{}
+	mux          *pat.PatternServeMux
+	errorEncoder = httpTransport.ServerErrorEncoder(api.EncodeError)
 )
 
 func main() {
+	flag.Parse()
 
-	var ID = "717f076e-e13c-45b4-bcc4-51c229e1b326"
-	var startDate = "2017-01-01"
-	var endDate = "2017-12-12"
+	// Configuración inicial
+	if err := config.Load(&cfg, configFile); err != nil {
+		fmt.Println(`CONFIG_LOAD_ERR: `, err)
+	}
+	if addr, ok := cfg.Server[`port`]; ok {
+		*listen = addr
+	}
 
-	fs := invoice.NewInvoiceService(ID)
-	total := fs.GetInvoice(startDate, endDate)
-	log.Print("TOTAL: ", total)
+	// loggers
+	logger := log.NewLogfmtLogger(os.Stderr)
+	logger = log.With(logger, "listen", *listen, "caller", log.DefaultCaller)
+
+	// Enpoints
+	ViewInvoiceHandler := httpTransport.NewServer(
+		ep.MakeReadInvoiceEndpoint(invSvc),
+		ep.DecodeReadInoviceRequest,
+		api.EncodeResponse,
+		errorEncoder,
+	)
+
+	// Rutas
+	mux = pat.New()
+	mux.Get(`/v1/invoice/:id/:startDate/:endDate`, ViewInvoiceHandler)
+	http.Handle(`/`, mux)
+
+	logger.Log(`msg`, `SERVING`, `address`, *listen)
+	logger.Log(`err`, http.ListenAndServe(*listen, nil))
 }
